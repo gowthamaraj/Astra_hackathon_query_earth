f2f43a1379596f41ceb631e4ac7c7c1201b21a51
gowthamaraj <gowthamaraj360@gmail.com>
2019-03-16 23:25:00 +0530

    The webpage template created

--- dev/start.py
+++ dev/start.py
@@ -4,28 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_there():
-    return 'Hello there!'
+    return render_template("index.html")
 
+@app.route('/index')
+def comp():
+    return render_template("index.html")
 
-@app.route('/form', methods=['POST', 'GET'])
-def bio_data_form():
-    if request.method == "POST":
-        username = request.form['username']
-        age = request.form['age']
-        email = request.form['email']
-        hobbies = request.form['hobbies']
-        return redirect(url_for('showbio',username=username,age=age, email=email,hobbies=hobbies))
-    return render_template("bio_form.html")
+@app.route('/search')
+def sear():
+    return render_template("search.html")
 
-
-@app.route('/showbio', methods=['GET'])
-def showbio():
-    username = request.args.get('username')
-    age = request.args.get('age')
-    email = request.args.get('email')
-    hobbies = request.args.get('hobbies')
-    return render_template("show_bio.html",username=username,age=age,email=email, hobbies=hobbies)
-
-
+@app.route('/trend')
+def tren():
+    return render_template("trend.html")
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
